65534d931f78835008125431b121f22136e4fda5
Alex Nio <volpenio@gmail.com>
2022-06-03 15:50:35 +0300

    fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -479,47 +479,16 @@ function clearStyle() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function($, jQuery) {// const swiperTop = new Swiper('.top-slider-container', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     slidesPerView: 1,
-//     // speed: 650,
-//     loop: true,
-//     // autoplay: true,
-//     // effect: "flip",
-//     // If we need pagination
-//     // pagination: {
-//     //   el: '.swiper-pagination',
-//     // },
-//   });
-//   const swiperBottom = new Swiper('.bottom-slider-container', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     slidesPerView: 1,
-//     // speed: 650,
-//     loop: true,
-//     // autoplay: true,
-//     // effect: "flip",
-//     // If we need pagination
-//     // pagination: {
-//     //   el: '.swiper-pagination',
-//     // },
-//   });
-$(function () {
+/* WEBPACK VAR INJECTION */(function($, jQuery) {$(function () {
   var duration = 500;
   var slides = $(".slider .block").length;
-  var i = 1; // $.each($('.slider .block'), function (i, el) {        
-  //   $(el).addClass('active');
-  // });
-  // Slide the images
+  var i = 1; // Slide the images
 
   function slide() {
     if (i <= slides) {
-      var imagelocation = ".slider .block:nth-child(" + i + ") img"; // var navlocation = ".nav a:nth-child(" + i + ")";
-
+      var imagelocation = ".slider .block:nth-child(" + i + ") img";
       $(imagelocation).siblings().removeClass("active animated flipInX");
-      $(imagelocation).addClass("active animated flipInX"); // $(navlocation).siblings().removeClass("navActive");
-      // $(navlocation).addClass("navActive");
+      $(imagelocation).addClass("active animated flipInX");
     }
 
     if (i == 0) {
@@ -529,34 +498,7 @@ $(function () {
     if (i < 0) {
       i = 0;
     }
-  } // Add navigation blips
-  // var blips = 0;
-  // for (var nav = 0; nav < slides; nav++) {
-  //   $(".nav").append('<a href="#"></a>');
-  // }
-  // $(".nav a:first-child").addClass("navActive");
-  // Configure the next/prev buttons
-  // $('.next').click(function () {
-  //   clearInterval(timer);
-  //   if (i == slides) {
-  //     i = 1;
-  //   } else {
-  //     i++
-  //   }
-  //   slide();
-  //   console.log(i);
-  // })
-  // $('.prev').click(function () {
-  //   clearInterval(timer);
-  //   if (i == 1) {
-  //     i = slides;
-  //   } else {
-  //     i--
-  //   }
-  //   slide();
-  //   console.log(i);
-  // })
-  // Autoplay
+  } // Autoplay
 
 
   timer = setInterval(function () {
@@ -609,7 +551,71 @@ function hoverHandler(e) {
       elem.classList.remove("active");
     }
   }
-}
+} // Slide checking and rotating
+// Получаем нужный элемент
+
+
+var element = document.querySelector('.block');
+var block;
+
+var Visible = function Visible(target) {
+  // Все позиции элемента
+  var targetPosition = {
+    top: window.pageYOffset + target.getBoundingClientRect().top,
+    left: window.pageXOffset + target.getBoundingClientRect().left,
+    right: window.pageXOffset + target.getBoundingClientRect().right,
+    bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+  },
+      // Получаем позиции окна
+  windowPosition = {
+    top: window.pageYOffset,
+    left: window.pageXOffset,
+    right: window.pageXOffset + document.documentElement.clientWidth,
+    bottom: window.pageYOffset + document.documentElement.clientHeight
+  };
+
+  if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+  targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+  targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+  targetPosition.left < windowPosition.right) {
+    // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+    console.clear();
+    console.log('Вы видите элемент :)');
+    rotateToBackSide('.square-flip');
+  } else {
+    // Если элемент не видно, то запускаем этот код
+    console.clear();
+  }
+
+  ; // 1. Step
+
+  function rotateToBackSide(selector) {
+    block = document.querySelectorAll(selector);
+    setTimeout(function () {
+      block.forEach(function (el) {
+        el.classList.add('active');
+      });
+      setTimeout(function () {
+        rotateToDefault(block);
+      }, 2000);
+    }, 2000);
+  } // 2. Step
+
+
+  function rotateToDefault(selector) {
+    selector.forEach(function (element) {
+      element.classList.remove('active');
+    });
+  }
+}; // Запускаем функцию при прокрутке страницы
+
+
+window.addEventListener('scroll', function () {
+  Visible(element);
+}); // А также запустим функцию сразу. А то вдруг, элемент изначально видно
+
+Visible(element);
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"), __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
--- src/blocks/modules/sliders/sliders.js
+++ src/blocks/modules/sliders/sliders.js
@@ -1,144 +1,66 @@
-// const swiperTop = new Swiper('.top-slider-container', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     slidesPerView: 1,
-//     // speed: 650,
-//     loop: true,
-//     // autoplay: true,
-//     // effect: "flip",
-//     // If we need pagination
-//     // pagination: {
-//     //   el: '.swiper-pagination',
-//     // },
-//   });
-
-//   const swiperBottom = new Swiper('.bottom-slider-container', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     slidesPerView: 1,
-//     // speed: 650,
-//     loop: true,
-//     // autoplay: true,
-//     // effect: "flip",
-//     // If we need pagination
-//     // pagination: {
-//     //   el: '.swiper-pagination',
-//     // },
-//   });
-
-
 $(function () {
 
-  var duration = 500;
-  var slides = $(".slider .block").length;
-  var i = 1;
+	var duration = 500;
+	var slides = $(".slider .block").length;
+	var i = 1;
 
-    // $.each($('.slider .block'), function (i, el) {        
-    //   $(el).addClass('active');
-    // });
+	// Slide the images
 
-  // Slide the images
+	function slide() {
+		if (i <= slides) {
+			var imagelocation = ".slider .block:nth-child(" + i + ") img";
 
-  function slide() {
-    if (i <= slides) {
+			$(imagelocation).siblings().removeClass("active animated flipInX");
+			$(imagelocation).addClass("active animated flipInX");
 
-      var imagelocation = ".slider .block:nth-child(" + i + ") img";
-      // var navlocation = ".nav a:nth-child(" + i + ")";
+		}
+		if (i == 0) {
+			i = slides;
+		}
+		if (i < 0) {
+			i = 0;
+		}
+	}
 
-      $(imagelocation).siblings().removeClass("active animated flipInX");
-      $(imagelocation).addClass("active animated flipInX");
+	// Autoplay
 
-      // $(navlocation).siblings().removeClass("navActive");
-      // $(navlocation).addClass("navActive");
+	timer = setInterval(function () {
+		i++;
 
-    }
-    if (i == 0) {
-      i = slides;
-    }
-    if (i < 0) {
-      i = 0;
-    }
-  }
-
-  // Add navigation blips
-
-  // var blips = 0;
-
-  // for (var nav = 0; nav < slides; nav++) {
-  //   $(".nav").append('<a href="#"></a>');
-  // }
-
-  // $(".nav a:first-child").addClass("navActive");
-
-  // Configure the next/prev buttons
-
-  // $('.next').click(function () {
-
-  //   clearInterval(timer);
-
-  //   if (i == slides) {
-  //     i = 1;
-  //   } else {
-  //     i++
-  //   }
-
-  //   slide();
-  //   console.log(i);
-  // })
-
-  // $('.prev').click(function () {
-
-  //   clearInterval(timer);
-
-  //   if (i == 1) {
-  //     i = slides;
-  //   } else {
-  //     i--
-  //   }
-
-  //   slide();
-  //   console.log(i);
-  // })
-
-  // Autoplay
-
-  timer = setInterval(function () {
-    i++;
-
-    if (i > slides) {
-      i = 1;
-    }
-    slide();
-  }, duration);
+		if (i > slides) {
+			i = 1;
+		}
+		slide();
+	}, duration);
 });
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-  //Count nr. of square classes
-  var countSquare = $('.front-to-right').length;
-  var countSquare2 = $('.front-to-left').length;
+	//Count nr. of square classes
+	var countSquare = $('.front-to-right').length;
+	var countSquare2 = $('.front-to-left').length;
 
-  //For each Square found add BG image
-  for (i = 0; i < countSquare; i++) {
-    var firstImage1 = $('.front-to-right').eq([i]);
-    var secondImage1 = $('.back-to-left').eq([i]);
-    
-    var getImage = firstImage1.attr('data-image');
-    var getImage2 = secondImage1.attr('data-image');
-    
-    firstImage1.css('background-image', 'url(' + getImage + ')');
-    secondImage1.css('background-image', 'url(' + getImage2 + ')');
-  }
-  
-  for (i = 0; i < countSquare2; i++) {
-    
-    var firstImage2 = $('.front-to-left').eq([i]);
-    var secondImage2 = $('.back-to-right').eq([i]);
-    var getImage = firstImage2.attr('data-image');
-    var getImage2 = secondImage2.attr('data-image');
-    firstImage2.css('background-image', 'url(' + getImage + ')');
-    secondImage2.css('background-image', 'url(' + getImage2 + ')');
-  }
+	//For each Square found add BG image
+	for (i = 0; i < countSquare; i++) {
+		var firstImage1 = $('.front-to-right').eq([i]);
+		var secondImage1 = $('.back-to-left').eq([i]);
+
+		var getImage = firstImage1.attr('data-image');
+		var getImage2 = secondImage1.attr('data-image');
+
+		firstImage1.css('background-image', 'url(' + getImage + ')');
+		secondImage1.css('background-image', 'url(' + getImage2 + ')');
+	}
+
+	for (i = 0; i < countSquare2; i++) {
+
+		var firstImage2 = $('.front-to-left').eq([i]);
+		var secondImage2 = $('.back-to-right').eq([i]);
+		var getImage = firstImage2.attr('data-image');
+		var getImage2 = secondImage2.attr('data-image');
+		firstImage2.css('background-image', 'url(' + getImage + ')');
+		secondImage2.css('background-image', 'url(' + getImage2 + ')');
+	}
 });
 
 
@@ -146,18 +68,88 @@ let cards = document.querySelectorAll('.square-flip');
 
 
 for (var i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('mouseenter', hoverHandler, false);
-  cards[i].addEventListener('mouseleave', hoverHandler, false);
+	cards[i].addEventListener('mouseenter', hoverHandler, false);
+	cards[i].addEventListener('mouseleave', hoverHandler, false);
 }
 
 function hoverHandler(e) {
-        for (var i = 0; i < cards.length; i++) {
-          const elem = cards[i];
+	for (var i = 0; i < cards.length; i++) {
+		const elem = cards[i];
 
-          if (e.type == "mouseenter"){
-            elem.classList.add("active");
-          } else {
-            elem.classList.remove("active");
-          }
-        }
+		if (e.type == "mouseenter") {
+			elem.classList.add("active");
+		} else {
+			elem.classList.remove("active");
+		}
+	}
 }
+
+// Slide checking and rotating
+
+// Получаем нужный элемент
+var element = document.querySelector('.block');
+let block;
+
+var Visible = function (target) {
+	// Все позиции элемента
+	var targetPosition = {
+		top: window.pageYOffset + target.getBoundingClientRect().top,
+		left: window.pageXOffset + target.getBoundingClientRect().left,
+		right: window.pageXOffset + target.getBoundingClientRect().right,
+		bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+	},
+		// Получаем позиции окна
+		windowPosition = {
+			top: window.pageYOffset,
+			left: window.pageXOffset,
+			right: window.pageXOffset + document.documentElement.clientWidth,
+			bottom: window.pageYOffset + document.documentElement.clientHeight
+		};
+
+	if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+		targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+		targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+		targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+		// Если элемент полностью видно, то запускаем следующий код
+		console.clear();
+		console.log('Вы видите элемент :)');
+
+		rotateToBackSide('.square-flip');
+
+	} else {
+		// Если элемент не видно, то запускаем этот код
+		console.clear();
+	};
+
+	// 1. Step
+	function rotateToBackSide(selector) {
+		block = document.querySelectorAll(selector);
+
+
+		setTimeout(function () {
+			block.forEach(el => {
+				el.classList.add('active');
+			});
+
+			setTimeout(function () {
+				rotateToDefault(block);
+			}, 2000);
+
+		}, 2000);
+	}
+
+	// 2. Step
+	function rotateToDefault(selector) {
+		selector.forEach(element => {
+			element.classList.remove('active');
+		});
+	}
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener('scroll', function () {
+	Visible(element);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible(element);
